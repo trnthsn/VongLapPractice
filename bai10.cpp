b1aad244a358ad2cee3c23f1b06841f3e144c77f
TrnThSn <saophaisoan23112002@gmail.com>
2021-03-24 00:01:31 +0700

    passed

---
+++ bai10.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> arr;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    sort (arr.begin(), arr.end());
+    int min = arr[1] - arr[0];
+    for (int i = 0; i < n - 1; i += 1)
+    {
+    	if (arr[i + 1] - arr[i] < min)
+    		min = arr[i + 1] - arr[i];
+    }
+    cout << min;
+}
